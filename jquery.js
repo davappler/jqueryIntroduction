043f5cdf0b405f72e696c599518effb709810d5b
Davinder Singh 2U <davindersingh2u@Davinders-MacBook-Pro-2.local>
2022-12-13 18:01:07 +0000

    removed one line

--- jquery.js
+++ jquery.js
@@ -5,4 +5,3 @@ titleEl.attr('class', 'fancy');
 titleEl.addClass('p-5'); //  - (`p-5` is for padding)
 titleEl.css('border', 'rgb(122, 242, 242) 5px solid');
 rootEl.append(titleEl);
-rootEl.append('<h2>With jQuery we can:</h2>');
--- script.js
+++ script.js
@@ -5,4 +5,3 @@ titleEl.className = 'fancy';
 titleEl.classList.add('p-5') //  - (`p-5` is for padding)
 titleEl.style.border = "rgb(122, 242, 242) 3px solid";
 rootEl.appendChild(titleEl);
-titleEl.append("Welcome to jQuery");
